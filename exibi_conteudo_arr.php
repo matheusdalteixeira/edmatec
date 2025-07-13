eb72c3383aa544cc0af9e3814142f644012ecf30
Matheus de Ávila Lins Teixeira <matheus.dal.teixeira@gmail.com>
2025-07-13 10:03:24 -0300

    Update exibi_conteudo_arr.php

--- exibi_conteudo_arr.php
+++ exibi_conteudo_arr.php
@@ -2,24 +2,24 @@
 
 	function exibi_conteudo_arr($p){
 
-	    global $v_vars, $t_vars;
-
-	    $v_vars = [$p];
-
-	    $t_vars = ["array"];
+		global $v_vars, $t_vars;
+		
+		$v_vars = [$p];
+		
+		$t_vars = ["array"];
 
 		if(valida_seq_tipo($v_vars, $t_vars)){
-
+		
 			$a = -1;
-
+		
 			while(isset($p[++$a]))
-
+		
 				echo PHP_EOL."[".$a."]".$p[$a];
-
+		
 		}
-
+		
 		else
-
+		
 			return false;
 
 	}
