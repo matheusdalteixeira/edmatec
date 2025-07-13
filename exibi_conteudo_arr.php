04b47cfa2a4b634028abf58d25b20dba804c40ba
Matheus de Ávila Lins Teixeira <matheus.dal.teixeira@gmail.com>
2025-07-13 05:06:18 -0300

    exibição dos indices de um array padronizadamente

---
+++ exibi_conteudo_arr.php
@@ -0,0 +1,27 @@
+<?php
+
+	function exibi_conteudo_arr($p){
+
+	    global $v_vars, $t_vars;
+
+	    $v_vars = [$p];
+
+	    $t_vars = ["array"];
+
+		if(valida_seq_tipo($v_vars, $t_vars)){
+
+			$a = -1;
+
+			while(isset($p[++$a]))
+
+				echo PHP_EOL."[".$a."]".$p[$a];
+
+		}
+
+		else
+
+			return false;
+
+	}
+
+?>
